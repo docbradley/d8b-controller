464ad728c9ecbe9e7800da4ea9496c3638ea377a
Adam D. Bradley <adam.d.bradley@gmail.com>
2014-10-17 00:10:27 -0700

    Demo "Bridge" application tethering some MCU and D8B controls

---
+++ d8b-controller/src/main/java/com/adamdbradley/demo/bridge/Bridge.java
@@ -0,0 +1,238 @@
+package com.adamdbradley.demo.bridge;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.adamdbradley.d8b.actor.FullBoot;
+import com.adamdbradley.d8b.actor.Reset;
+import com.adamdbradley.d8b.audio.AudioControlConnection;
+import com.adamdbradley.d8b.console.ConsoleControlConnection;
+import com.adamdbradley.d8b.console.signal.Signal;
+import com.adamdbradley.mcu.MCUMidiPort;
+import com.adamdbradley.mcu.MidiPortHelper;
+import com.adamdbradley.mcu.actor.MCUReset;
+import com.adamdbradley.mcu.actor.MasterStartup;
+import com.adamdbradley.mcu.console.Fader;
+import com.adamdbradley.mcu.console.protocol.command.MoveFader;
+import com.adamdbradley.mcu.console.protocol.signal.ButtonMessage;
+import com.adamdbradley.mcu.console.protocol.signal.ButtonPressed;
+import com.adamdbradley.mcu.console.protocol.signal.ChannelButtonMessage;
+import com.adamdbradley.mcu.console.protocol.signal.ChannelVPotMoved;
+import com.adamdbradley.mcu.console.protocol.signal.FaderMoved;
+import com.adamdbradley.mcu.console.protocol.signal.FaderReleased;
+
+public class Bridge
+implements Runnable, AutoCloseable {
+
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(8);
+
+    private final MCUMidiPort mcuPort;
+    private final ConsoleControlConnection console;
+    private final AudioControlConnection audio;
+
+    private final Queue<Signal> consoleQueue = new ConcurrentLinkedQueue<>();
+    private final Queue<com.adamdbradley.mcu.console.protocol.Signal> mcuQueue = new ConcurrentLinkedQueue<>();
+
+    private final Model model = new Model();
+
+
+    public Bridge() throws Exception {
+        console = new ConsoleControlConnection("COM12");
+        audio = new AudioControlConnection("COM14");
+        mcuPort = MidiPortHelper.findAndOpenPort();
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            reallyRun();
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void reallyRun() throws Exception {
+        final List<Future<?>> pending = new ArrayList<>(64);
+
+        final Future<?> d8bBoot = executor.submit(new FullBoot(console, audio));
+        final Future<?> mcuBoot = executor.submit(new MasterStartup(mcuPort));
+        pending.add(d8bBoot);
+        pending.add(mcuBoot);
+
+        while (!pending.isEmpty()) {
+            final Iterator<Future<?>> it = pending.iterator();
+            try {
+                it.next().get();
+            } catch (ExecutionException e) {
+                throw (Exception) e.getCause();
+            }
+            it.remove();
+        }
+
+        executor.submit(new Reset(console));
+        executor.submit(new MCUReset(mcuPort));
+
+        console.subscribe(consoleQueue);
+        mcuPort.subscribe(mcuQueue);
+
+        model.connect(console, mcuPort);
+
+        executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                processConsole(consoleQueue);
+            }
+        }, 0, 20, TimeUnit.MILLISECONDS);
+        executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                processMCU(mcuQueue);
+            }
+        }, 10, 20, TimeUnit.MILLISECONDS);
+
+        Thread.sleep(120000);
+
+        executor.shutdown();
+    }
+
+
+    private void processConsole(final Queue<Signal> consoleQueue) {
+        try {
+            while (!consoleQueue.isEmpty()) {
+                final Signal signal = consoleQueue.poll();
+                if (signal == null) {
+                    return;
+                }
+                switch (signal.type) {
+                case Heartbeat1:
+                case Heartbeat2:
+                    break;
+                default:
+                    System.out.println("CONSOLE: " + signal);
+                }
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+
+    private void processMCU(final Queue<com.adamdbradley.mcu.console.protocol.Signal> mcuQueue) {
+        try {
+            while (!mcuQueue.isEmpty()) {
+                final com.adamdbradley.mcu.console.protocol.Signal signal = mcuQueue.poll();
+
+                if (signal == null) {
+                    return;
+                }
+    
+                if (signal instanceof ButtonPressed) {
+                    switch (((ButtonMessage) signal).button) {
+                    case BankLeft:
+                        model.mcuMove(-8);
+                        break;
+                    case BankRight:
+                        model.mcuMove(8);
+                        break;
+                    case ChannelLeft:
+                        model.mcuMove(-1);
+                        break;
+                    case ChannelRight:
+                        model.mcuMove(1);
+                        break;
+                    case Rewind:
+                        model.rewind(signal instanceof ButtonPressed);
+                        break;
+                    case FastFwd:
+                        model.fastFwd(signal instanceof ButtonPressed);
+                        break;
+                    case Stop:
+                        model.stop(signal instanceof ButtonPressed);
+                        break;
+                    case Play:
+                        model.play(signal instanceof ButtonPressed);
+                        break;
+                    case Record:
+                        model.record(signal instanceof ButtonPressed);
+                        break;
+                    default:
+                        return;
+                    }
+                } else if (signal instanceof ChannelButtonMessage) {
+                    switch (((ChannelButtonMessage) signal).channelButton) {
+                    case REC:
+                        model.channelRec(model.mcuViewOffset + ((ChannelButtonMessage) signal).channel.ordinal());
+                        break;
+                    case SOLO:
+                        model.channelSolo(model.mcuViewOffset + ((ChannelButtonMessage) signal).channel.ordinal());
+                        break;
+                    case MUTE:
+                        model.channelMute(model.mcuViewOffset + ((ChannelButtonMessage) signal).channel.ordinal());
+                        break;
+                    case SELECT:
+                        model.channelSelect(model.mcuViewOffset + ((ChannelButtonMessage) signal).channel.ordinal());
+                        break;
+                    case VPot:
+                        model.channelVPot(model.mcuViewOffset + ((ChannelButtonMessage) signal).channel.ordinal());
+                        break;
+                    default:
+                        throw new IllegalArgumentException();
+                    }
+                } else if (signal instanceof ChannelVPotMoved) {
+                    model.channelVPot(model.mcuViewOffset + ((ChannelVPotMoved) signal).channel.ordinal(),
+                            ((ChannelVPotMoved) signal).velocity);
+                } else if (signal instanceof FaderMoved) {
+                    final FaderMoved faderSignal = (FaderMoved) signal;
+                    if (faderSignal.fader == Fader.Master) {
+                        model.masterFader(faderSignal.value);
+                    } else {
+                        model.fader(model.mcuViewOffset + faderSignal.fader.ordinal(), faderSignal.value);
+                    }
+                } else if (signal instanceof FaderReleased) {
+                    final FaderReleased faderSignal = (FaderReleased) signal;
+                    if (faderSignal.fader == Fader.Master) {
+                        mcuPort.send(new MoveFader(faderSignal.fader, model.masterFader));
+                    } else {
+                        mcuPort.send(new MoveFader(faderSignal.fader, model.channels[faderSignal.fader.ordinal() + model.mcuViewOffset].fader));
+                    }
+                }
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        try {
+            mcuPort.close();
+        } finally {
+            try {
+                console.close();
+            } finally {
+                audio.close();
+            }
+        }
+    }
+
+
+
+    public static void main(final String[] argv) throws Exception {
+        try (final Bridge bridge = new Bridge()) {
+            bridge.run();
+        }
+    }
+
+}
---
+++ d8b-controller/src/main/java/com/adamdbradley/demo/bridge/Model.java
@@ -0,0 +1,300 @@
+package com.adamdbradley.demo.bridge;
+
+import com.adamdbradley.d8b.console.ConsoleControlConnection;
+import com.adamdbradley.d8b.console.PanelLED;
+import com.adamdbradley.d8b.console.command.LightPanelLED;
+import com.adamdbradley.d8b.console.command.ShutPanelLED;
+import com.adamdbradley.mcu.MCUMidiPort;
+import com.adamdbradley.mcu.console.ChannelLED;
+import com.adamdbradley.mcu.console.DeviceType;
+import com.adamdbradley.mcu.console.Fader;
+import com.adamdbradley.mcu.console.protocol.command.LightChannelLED;
+import com.adamdbradley.mcu.console.protocol.command.MoveFader;
+import com.adamdbradley.mcu.console.protocol.command.ShutChannelLED;
+import com.adamdbradley.mcu.console.protocol.command.WriteScreen;
+import com.adamdbradley.mcu.console.protocol.command.WriteVPot;
+import com.adamdbradley.mcu.console.protocol.command.WriteVPot.VPotMode;
+
+public class Model {
+
+    public int d8bViewOffset = 0;
+    public int mcuViewOffset = 0;
+
+
+
+    private ConsoleControlConnection console;
+    private MCUMidiPort mcuPort;
+
+    public void connect(ConsoleControlConnection console, MCUMidiPort mcuPort) {
+        this.console = console;
+        this.mcuPort = mcuPort;
+    }
+
+    public final Channel[] channels;
+
+    public int masterVpotValue;
+    public VPotMode masterVpotMode;
+    public int masterFader;
+
+    public Model() {
+        channels = new Channel[48];
+        for (int i=0; i<channels.length; i++) {
+            channels[i] = new Channel();
+        }
+    }
+
+    public class Channel {
+        public int vpotValue;
+        public VPotMode vpotMode = VPotMode.SinglePosition;
+        public boolean record;
+        public boolean assign;
+        public boolean write;
+        public boolean green;
+        public boolean red;
+        public boolean select;
+        public boolean solo;
+        public boolean mute;
+
+        // 0 - 1023
+        public int fader;
+    }
+
+    public boolean record;
+    public TransportMode transportMode = TransportMode.Stopped;
+    public enum TransportMode {
+        Stopped,
+        Playing,
+        Paused,
+        Shuttling,
+        Rewinding,
+        FastForwarding
+    }
+
+    public void rewind(boolean b) {
+        if (b) {
+            if (transportMode == TransportMode.Stopped) {
+                transportMode = TransportMode.Rewinding;
+                displayTransportMode();
+            }
+        } else {
+            if (transportMode == TransportMode.Rewinding) {
+                transportMode = TransportMode.Stopped;
+                displayTransportMode();
+            }
+        }
+    }
+
+    public void fastFwd(boolean b) {
+        if (b) {
+            if (transportMode == TransportMode.Stopped) {
+                transportMode = TransportMode.FastForwarding;
+                displayTransportMode();
+            }
+        } else {
+            if (transportMode == TransportMode.FastForwarding) {
+                transportMode = TransportMode.Stopped;
+                displayTransportMode();
+            }
+        }
+    }
+
+    public void stop(boolean b) {
+        if (b) {
+            transportMode = TransportMode.Stopped;
+            displayTransportMode();
+        }
+    }
+
+    public void play(boolean b) {
+        if (b) {
+            if (transportMode == TransportMode.Stopped || transportMode == TransportMode.Paused) {
+                transportMode = TransportMode.Playing;
+                displayTransportMode();
+            } else if (transportMode == TransportMode.Playing) {
+                transportMode = TransportMode.Paused;
+                displayTransportMode();
+            }
+        }
+    }
+
+    public void record(boolean b) {
+        if (b) {
+            this.record = !this.record;
+            displayTransportMode();
+        }
+    }
+
+    private void displayTransportMode() {
+        if (record) {
+            console.send(new LightPanelLED(PanelLED.vtransportproxy_RecWrite));
+            mcuPort.send(new com.adamdbradley.mcu.console.protocol.command.LightPanelLED(com.adamdbradley.mcu.console.PanelLED.Record));
+        } else {
+            console.send(new ShutPanelLED(PanelLED.vtransportproxy_RecWrite));
+            mcuPort.send(new com.adamdbradley.mcu.console.protocol.command.ShutPanelLED(com.adamdbradley.mcu.console.PanelLED.Record));
+        }
+
+        switch (transportMode) {
+        case Rewinding:
+            // Rew lit
+        case FastForwarding:
+            // FF lit
+        case Paused:
+            // Play blinking (or just lit if blink not native)
+        case Playing:
+            // Play lit
+        case Shuttling:
+            // Stop lit
+        case Stopped:
+            // Nothing
+        }
+    }
+
+    public void channelRec(int i) {
+        channels[i].record = !channels[i].record;
+        displayChannel(i);
+    }
+
+    public void channelSolo(int i) {
+        channels[i].solo = !channels[i].solo;
+        displayChannel(i);
+    }
+
+    public void channelMute(int i) {
+        channels[i].mute = !channels[i].mute;
+        displayChannel(i);
+    }
+
+    public void channelSelect(int i) {
+        channels[i].select = !channels[i].select;
+        displayChannel(i);
+    }
+
+    public void channelVPot(int i) {
+        channels[i].vpotMode = VPotMode.values()[(channels[i].vpotMode.ordinal() + 1) % VPotMode.values().length];
+        channels[i].vpotValue = 0;
+        displayChannel(i);
+    }
+
+    public void channelVPot(int i, int velocity) {
+        if (velocity > 0) {
+            channels[i].vpotValue++;
+        } else {
+            channels[i].vpotValue--;
+        }
+        displayChannel(i);
+    }
+
+    public void mcuMove(int adj) {
+        mcuViewOffset = Math.max(0,
+                Math.min(
+                        channels.length - 8,
+                        mcuViewOffset + adj));
+        System.err.println("MCU adv: " + mcuViewOffset);
+        for (int i=0; i<8; i++) {
+            displayChannelMCU(mcuViewOffset + i);
+        }
+    }
+
+    /**
+     * Range 0 - 1023
+     * @param i
+     * @param value
+     */
+    public void fader(int i, int value) {
+        if (channels[i].fader != value) {
+            System.err.println("Moving " + i);
+            channels[i].fader = value;
+            if (inViewD8B(i)) {
+                console.send(new com.adamdbradley.d8b.console.command.MoveFader(com.adamdbradley.d8b.console.Fader.values()[i - d8bViewOffset], value >> 2));
+            }
+            if (inViewMCU(i)) {
+                mcuPort.send(new MoveFader(Fader.values()[i - mcuViewOffset], value));
+            }
+        }
+    }
+
+    public void masterFader(int value) {
+        if (masterFader != value) {
+            masterFader = value;
+            console.send(new com.adamdbradley.d8b.console.command.MoveFader(com.adamdbradley.d8b.console.Fader.Master, value >> 2));
+            mcuPort.send(new MoveFader(Fader.Master, value));
+        }
+    }
+
+    private boolean inViewMCU(final int i) {
+        return (i >= mcuViewOffset && i < mcuViewOffset + 8);
+    }
+
+    private boolean inViewD8B(final int i) {
+        return (i >= d8bViewOffset && i < d8bViewOffset + 24);
+    }
+
+
+
+
+
+
+
+    private void displayChannel(int i) {
+        if (inViewMCU(i)) {
+            displayChannelMCU(i);
+        }
+        if (inViewD8B(i)) {
+            displayChannelD8B(i);
+            
+        }
+    }
+
+    private void displayChannelMCU(int modelChannel) {
+        final Model.Channel channel = channels[modelChannel];
+
+        final int pos = modelChannel - mcuViewOffset;
+
+        final com.adamdbradley.mcu.console.Channel strip = com.adamdbradley.mcu.console.Channel.values()[pos];
+        final Fader fader = strip.fader();
+
+        mcuPort.send(new WriteScreen(DeviceType.Master,
+                0, pos * 7, "       "));
+        mcuPort.send(new WriteScreen(DeviceType.Master,
+                0, pos * 7, Integer.toString(modelChannel)));
+
+        final String status = ""
+                + (channel.assign ? 'A' : ' ')
+                + (channel.green ? (channel.red ? 'X' : '^') : (channel.record ? 'v' : ' '))
+                + (channel.write ? 'W' : ' ')
+                ;
+        mcuPort.send(new WriteScreen(DeviceType.Master, 1, pos * 7, status));
+
+        mcuPort.send(new MoveFader(fader, channel.fader));
+        if (channel.record) {
+            mcuPort.send(new LightChannelLED(strip, ChannelLED.REC));
+        } else {
+            mcuPort.send(new ShutChannelLED(strip, ChannelLED.REC));
+        }
+        if (channel.solo) {
+            mcuPort.send(new LightChannelLED(strip, ChannelLED.SOLO));
+        } else {
+            mcuPort.send(new ShutChannelLED(strip, ChannelLED.SOLO));
+        }
+        if (channel.mute) {
+            mcuPort.send(new LightChannelLED(strip, ChannelLED.MUTE));
+        } else {
+            mcuPort.send(new ShutChannelLED(strip, ChannelLED.MUTE));
+        }
+        if (channel.select) {
+            mcuPort.send(new LightChannelLED(strip, ChannelLED.SELECT));
+        } else {
+            mcuPort.send(new ShutChannelLED(strip, ChannelLED.SELECT));
+        }
+        mcuPort.send(new WriteVPot(strip, channel.vpotMode, channel.vpotValue, false));
+    }
+
+    private void displayChannelD8B(int i) {
+        final Model.Channel channel = channels[mcuViewOffset + i];
+
+        final com.adamdbradley.d8b.console.Fader fader = com.adamdbradley.d8b.console.Fader.values()[i - d8bViewOffset];
+        console.send(new com.adamdbradley.d8b.console.command.MoveFader(fader,
+                channel.fader >> 2));
+    }
+
+}
