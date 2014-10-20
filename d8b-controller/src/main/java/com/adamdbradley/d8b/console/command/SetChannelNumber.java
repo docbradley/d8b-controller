1cd0382cfef054dae51b3b30de538676f18593c4
Adam D. Bradley <adam.d.bradley@gmail.com>
2014-10-19 19:14:18 -0700

    Add additional validation to d8b SetChannelNumber

--- d8b-controller/src/main/java/com/adamdbradley/d8b/console/command/SetChannelNumber.java
+++ d8b-controller/src/main/java/com/adamdbradley/d8b/console/command/SetChannelNumber.java
@@ -1,9 +1,28 @@
 package com.adamdbradley.d8b.console.command;
 
+import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
+
 /**
  * Controls the two-digit "channel number" indicator.
  */
 public class SetChannelNumber extends Command {
+
+    public static final Set<Character> ALLOWED_CHARACTERS = ImmutableSet.of(
+            ' ',
+            'F',
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9'
+            );
 
     private final String value;
 
@@ -23,9 +42,16 @@ public class SetChannelNumber extends Command {
     public SetChannelNumber(final String value) {
         super(CommandType.SetChannelNumber);
         if (value.length() <1 || value.length() > 2) {
-            throw new RuntimeException("Value must be of length 1 or 2: [" + value + "]");
+            throw new IllegalArgumentException("Value must be of length 1 or 2: [" + value + "]");
         }
-        // XXX validate values are acceptable -- Digit, Dash, Space
+
+        if (!ALLOWED_CHARACTERS.contains(value.charAt(0))) {
+            throw new IllegalArgumentException("Illegal character [" + value.charAt(0) + "]");
+        }
+        if (value.length() > 1 && !ALLOWED_CHARACTERS.contains(value.charAt(1))) {
+            throw new IllegalArgumentException("Illegal character [" + value.charAt(1) + "]");
+        }
+
         this.value = value;
     }
 
